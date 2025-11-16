8067a35565ed1db02c62642330cf409e35858bc5
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-17 00:50:59 +0200

    create login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,167 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:scholar_chat_app/constants.dart';
+import 'package:scholar_chat_app/pages/signup_page.dart';
+import 'package:scholar_chat_app/widgets/custom_button.dart';
+
+import '../widgets/custom_text_field.dart';
+import 'chat_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  static const route = '/LoginPage';
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoaded = false;
+  String? email;
+
+  String? password;
+  final GlobalKey<FormState> formKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: isLoaded,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 180),
+                    Image.asset(
+                      'assets/images/scholar.png',
+                      width: 300,
+                      height: 80,
+                    ),
+                    Text(
+                      'Scholar Chat',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Pacifico',
+                        color: vPrimaryColor,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400,
+                        color: vPrimaryColor,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(height: 20),
+                    CustomTextField(hintTxt: 'Email', onChanged: (data) {
+                      email = data;
+                    }),
+                    SizedBox(height: 7),
+                    CustomTextField(
+                      showTxt: true,
+                      hintTxt: 'Password',
+                      onChanged: (data) {
+                        password = data;
+                      },
+                    ),
+                    SizedBox(height: 12),
+                    CustomButton(txt: 'Sign In', onTap: () async {
+                      if(formKey.currentState!.validate()){
+                        isLoaded = true;
+                        setState(() {});
+                        try {
+                          await signAuth();
+                          showSnackBar(context, 'Success');
+                          Navigator.pushNamed(context, ChatPage.route , arguments: email);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            showSnackBar(context, e);
+                          } else if (e.code == 'wrong-password') {
+                            showSnackBar(context, e);
+                          }
+                          else
+                          {
+                            showSnackBar(context, e.message?? 'An unknown error occurred.');
+                          }
+                        }catch(e){
+                          showSnackBar(context, e);
+                        }
+                        isLoaded = false;
+                        setState(() {});
+                      }
+                    },
+                    ),
+                    // SizedBox(height: 5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "don't have an account?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: vPrimaryColor,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pushReplacementNamed(
+                            context,
+                            SignupPage.route,
+                          ),
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: vPrimaryColor,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> signAuth() async {
+    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email!,
+        password: password!,
+    );
+  }
+}
+void showSnackBar(BuildContext context, e) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      elevation: 8,
+      padding: EdgeInsets.all(5),
+      backgroundColor: vPrimaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      content: Text(
+        e.toString(),
+        textAlign: TextAlign.center,
+        style: TextStyle(color: kPrimaryColor , fontWeight: FontWeight.w500),
+      ),
+    ),
+  );
+}
