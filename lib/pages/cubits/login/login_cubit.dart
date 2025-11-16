3c4210c84bd2b5b6cda4d57990c6e5a2ce3fdaad
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-17 01:06:12 +0200

    finish Login cubit

--- lib/pages/cubits/login/login_cubit.dart
+++ lib/pages/cubits/login/login_cubit.dart
@@ -7,18 +7,24 @@ part 'login_state.dart';
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
 
-  loginAuth({required String email , required String password}) async {
+  loginAuth({required String email, required String password}) async {
     emit(LoginLoading());
-    try{
-        final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-        emit(LoginSuccess());
-      } catch(e) {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(LoginSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
         emit(LoginFailure(errMessage: e.toString()));
+      } else if (e.code == 'wrong-password') {
+        emit(LoginFailure(errMessage: e.toString()));
+      } else {
+        emit(LoginFailure(errMessage: e.toString()));
+      }
+    } catch (e) {
+      emit(LoginFailure(errMessage: e.toString()));
     }
-
   }
-
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:scholar_chat_app/constants.dart';
+import 'package:scholar_chat_app/pages/cubits/login/login_cubit.dart';
 import 'package:scholar_chat_app/pages/signup_page.dart';
 import 'package:scholar_chat_app/widgets/custom_button.dart';
 
@@ -10,6 +11,7 @@ import 'chat_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
   static const route = '/LoginPage';
 
   @override
@@ -18,150 +20,141 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool isLoaded = false;
-  String? email;
-
-  String? password;
   final GlobalKey<FormState> formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isLoaded,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Form(
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if(state is LoginSuccess){
+          return showSnackBar(context, 'Success');
+        }
+        else if(state is LoginFailure){
+          return showSnackBar(context, state.errMessage);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: state is LoginLoading? true : false,
+          child: Scaffold(
+            backgroundColor: kPrimaryColor,
+            body: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
                   children: [
-                    SizedBox(height: 180),
-                    Image.asset(
-                      'assets/images/scholar.png',
-                      width: 300,
-                      height: 80,
-                    ),
-                    Text(
-                      'Scholar Chat',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w600,
-                        fontFamily: 'Pacifico',
-                        color: vPrimaryColor,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 30),
-                    Text(
-                      'Sign In',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400,
-                        color: vPrimaryColor,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                    SizedBox(height: 20),
-                    CustomTextField(hintTxt: 'Email', onChanged: (data) {
-                      email = data;
-                    }),
-                    SizedBox(height: 7),
-                    CustomTextField(
-                      showTxt: true,
-                      hintTxt: 'Password',
-                      onChanged: (data) {
-                        password = data;
-                      },
-                    ),
-                    SizedBox(height: 12),
-                    CustomButton(txt: 'Sign In', onTap: () async {
-                      if(formKey.currentState!.validate()){
-                        isLoaded = true;
-                        setState(() {});
-                        try {
-                          await signAuth();
-                          showSnackBar(context, 'Success');
-                          Navigator.pushNamed(context, ChatPage.route , arguments: email);
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            showSnackBar(context, e);
-                          } else if (e.code == 'wrong-password') {
-                            showSnackBar(context, e);
-                          }
-                          else
-                          {
-                            showSnackBar(context, e.message?? 'An unknown error occurred.');
-                          }
-                        }catch(e){
-                          showSnackBar(context, e);
-                        }
-                        isLoaded = false;
-                        setState(() {});
-                      }
-                    },
-                    ),
-                    // SizedBox(height: 5,),
-                    Row(
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
+                        SizedBox(height: 180),
+                        Image.asset(
+                          'assets/images/scholar.png',
+                          width: 300,
+                          height: 80,
+                        ),
                         Text(
-                          "don't have an account?",
+                          'Scholar Chat',
                           style: TextStyle(
-                            fontWeight: FontWeight.w500,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Pacifico',
                             color: vPrimaryColor,
                           ),
                           textAlign: TextAlign.center,
                         ),
-                        TextButton(
-                          onPressed: () => Navigator.pushReplacementNamed(
-                            context,
-                            SignupPage.route,
+                        SizedBox(height: 30),
+                        Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w400,
+                            color: vPrimaryColor,
                           ),
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              color: vPrimaryColor,
+                          textAlign: TextAlign.left,
+                        ),
+                        SizedBox(height: 20),
+                        CustomTextField(hintTxt: 'Email',
+                            onChanged: (data) {
+
+                            }
+                        ),
+                        SizedBox(height: 7),
+                        CustomTextField(
+                          showTxt: true,
+                          hintTxt: 'Password',
+                          onChanged: (data) {},
+                        ),
+                        SizedBox(height: 12),
+                        CustomButton(
+                          txt: 'Sign In',
+                          onTap: () async {
+                            if (formKey.currentState!.validate()) {
+                              Navigator.pushNamed(
+                                context,
+                                ChatPage.route,
+                                arguments: 'email',
+                              );
+                            }
+                          },
+                        ),
+                        // SizedBox(height: 5,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "don't have an account?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: vPrimaryColor,
+                              ),
+                              textAlign: TextAlign.center,
                             ),
-                            textAlign: TextAlign.center,
-                          ),
+                            TextButton(
+                              onPressed: () =>
+                                  Navigator.pushReplacementNamed(
+                                    context,
+                                    SignupPage.route,
+                                  ),
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: vPrimaryColor,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
+        );
+      },
+    );
+  }
+
+  void showSnackBar(BuildContext context, e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        elevation: 8,
+        padding: EdgeInsets.all(5),
+        backgroundColor: vPrimaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        content: Text(
+          e.toString(),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.w500),
         ),
       ),
     );
   }
-
-  Future<void> signAuth() async {
-    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email!,
-        password: password!,
-    );
-  }
-}
-void showSnackBar(BuildContext context, e) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      elevation: 8,
-      padding: EdgeInsets.all(5),
-      backgroundColor: vPrimaryColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      content: Text(
-        e.toString(),
-        textAlign: TextAlign.center,
-        style: TextStyle(color: kPrimaryColor , fontWeight: FontWeight.w500),
-      ),
-    ),
-  );
 }
